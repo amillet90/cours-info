ea3ffa881b3047b783804f5d7c65cb7185ae9574
amillet <amillet.iut90@gmail.com>
2025-01-21 07:23:36 +0100

    maj diw

---
+++ bdd2/tp/ressources/S2_Mysql_tp1_data_etu_insert.sql
@@ -0,0 +1,35 @@
+INSERT INTO LIGNE VALUES 
+(1,4,12),(1,9,4),(1,12,4),
+(1,15,4),(2,3,6),(2,6,6),
+(2,7,6),(2,14,6),(3,1,3),
+(3,11,3),(3,12,3),(3,16,3),
+(4,2,27),(4,8,5),(4,10,5),
+(4,17,5),(5,12,4),(6,11,3),
+(7,8,5),(8,14,6);
+
+INSERT INTO COMMANDE VALUES 
+(1,'2023-04-11',3),(2,'2023-12-11',2),(3,'2024-01-28',4),
+(4,'2023-09-19',1),(5,'2023-04-11',3),(6,'2023-12-11',2),
+(7,'2023-03-28',4),(8,'2023-12-09',1),(9,'2023-04-11',3),
+(10,'2023-12-11',2);
+
+INSERT INTO ARTICLE VALUES 
+(1,'Gâteaux chocolat ( pâtisserie)',5.25),(2,'gâteau fraise (pâtisserie) ',5.25),(3,'Mouchoir ( hygiène)',2.54),
+(4,'Coca cola ( boisson )',1.52),(5,'Salade ( légume ) ',2.00),(6,'Choux ( légume ) ',2.50),
+(7,'Pomme de terre ( légume ) ',1.50),(8,'Tomate ( légume ) ',1.50),(9,'Haricot ( légume ) ',6.50),
+(10,'Potiron ( légume ) ',3.00),(11,'Poireau ( légume ) ',1.50),(12,'Fenouil ( légume ) ',3.00),
+(13,'Pissenlit ( légume ) ',5.00),(14,'Petit pois ( légume ) ',6.00),(15,'Poivron ( légume ) ',3.00),
+(16,'Radis ( légume ) ',2.00),(17,'Pomme ( fruit ) ',3.00),(18,'Perrier ( 1L eau ) ',1.50),
+(19,'Vittel ( 1L eau ) ',1.00);
+
+INSERT INTO CLIENT VALUES 
+(1,'Mutz','Ruelisheim'),(2,'Rato','Besancon'),(3,'Goerig','Walheim'),
+(4,'Walter','Bretten'),(5,'PAUTOT','Belfort'),(6,'PETIT','Belfort'),
+(7,'SAINT DIZIER','Sevenans'),(8,'SALVI','Offemont'),(9,'TERRAT','Belfort'),
+(10,'TYRODE','Valdoie'),(11,'ALANKAYA','Bavilliers'),(12,'DAROSEY','Essert'),
+(13,'duguet','Belfort'),(14,'ESSENBURGER','Belfort'),(15,'JAOUEN','Sevenans'),
+(16,'molin','Belfort'),(17,'AMGHAR','Belfort'),(18,'BOUCHAUD','Belfort'),
+(19,'COTTARD','Belfort'),(20,'LAMOTTE','Belfort'),(21,'METTEY','Belfort'),
+(22,'WOLF','Belfort'),(23,'BISMUTH','Belfort'),(24,'chaillet','Belfort'),
+(25,'DECOCK','Belfort');
+
