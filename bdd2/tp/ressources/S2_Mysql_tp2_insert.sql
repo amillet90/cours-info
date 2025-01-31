362e77df3e36cf7a24b095ddbd4a6486e08b026e
amillet <amillet.iut90@gmail.com>
2025-01-31 07:04:49 +0100

    maj diw

---
+++ bdd2/tp/ressources/S2_Mysql_tp2_insert.sql
@@ -0,0 +1,192 @@
+INSERT INTO classement VALUES
+(2,1,1),
+(1,1,2),
+(4,1,3),
+(6,1,4),
+(5,1,5),
+(3,2,1),
+(2,2,2),
+(6,2,3),
+(4,2,4),
+(1,2,5),
+(5,2,6),
+(4,3,2),
+(5,3,3),
+(6,3,4),
+(2,4,2),
+(5,4,3),
+(6,4,4);
+
+INSERT INTO comporte VALUES
+(1,1),
+(1,2),
+(2,1),
+(2,2),
+(3,1),
+(3,2);
+
+INSERT INTO station  VALUES
+ (NULL,'Tignes',2000,'France'),
+ (NULL,'Verbier',2200,'suisse'),
+ (NULL,'Chambery',2300,'France'),
+ (NULL,'Zinal',2400,'suisse'),
+ (NULL,'Grimentz',2500,'suisse'),
+ (NULL,'Valoire',1900,'France'),
+ (NULL,'Vallemenier',1800,'France'),
+ (NULL,'La Roziere',1500,'France'),
+ (NULL,'Metabief',1200,'France'),
+ (NULL,'Le Ballon d\'alsace',1000,'France');
+
+INSERT INTO competition VALUES
+(NULL,'compet cadet france','2019-02-01',1),
+(NULL,'compet junior france','2019-02-01',1),
+(NULL,'competition cadet europe','2019-02-01',1),
+(NULL,'competition junior europe','2019-02-01',3),
+(NULL,'championnat cadet france','2019-02-01',1),
+(NULL,'championnat junior france','2019-02-01',2),
+(NULL,'championnat cadet europe','2019-02-01',1),
+(NULL,'championnat junior europe','2019-02-01',3);
+
+
+INSERT INTO skieur VALUES
+(NULL,'tom',1,1),
+(NULL,'pierre',2,1),
+(NULL,'paul',1,1),
+(NULL,'jacques',2,1),
+(NULL,'edouard',2,2),
+(NULL,'robert',2,3),
+(NULL,'alberto',1,9),
+(NULL,'bernard',2,10);
+
+
+INSERT INTO specialite VALUES
+(NULL, 'saut'),
+(NULL, 'fond'),
+(NULL, 'slalom'),
+(NULL, 'descente'),
+(NULL, 'free style');
+
+
+-- ------------------------------------------------
+-- version 2
+-- ------------------------------------------------
+
+INSERT INTO comporte VALUES
+(1,1),
+(1,2),
+(2,1),
+(2,2),
+(3,1),
+(3,2),
+(4,1),
+(4,2),
+(5,1),
+(5,2),
+(6,1),
+(6,2),
+(7,1),
+(7,2),
+(8,1),
+(8,2),
+(9,1),
+(9,2),
+(10,1),
+(10,2),
+(11,1),
+(11,2),
+(12,1),
+(12,2),
+(13,1),
+(13,2),
+(14,1),
+(14,2),
+(15,1),
+(15,2),
+(15,3),
+(15,4),
+(16,1),
+(16,2),
+(17,1),
+(17,2),
+(18,1),
+(18,2),
+(19,1),
+(19,2);
+
+
+INSERT INTO competition VALUES
+(NULL,'compet cadet france','2019-02-01',1),
+(NULL,'compet junior france','2019-02-01',1),
+(NULL,'competition cadet europe','2019-02-01',1),
+(NULL,'competition junior europe','2019-02-01',3),
+(NULL,'championnat cadet france','2019-02-01',1),
+(NULL,'championnat junior france','2019-02-01',2),
+(NULL,'championnat cadet europe','2019-02-01',1),
+(NULL,'championnat junior europe','2019-02-01',3),
+(NULL, 'compet cadet france 2020' ,2020-02-01,1),
+(NULL, 'compet junior france 2020' ,2020-02-01,1),
+(NULL, 'competition cadet europe 2020' ,2020-02-01,1),
+(NULL, 'competition junior europe 2020' ,2020-02-01,3),
+(NULL, 'championnat cadet france 2020' ,2020-02-01,1),
+(NULL, 'championnat junior france 2020' ,2020-02-01,2),
+(NULL, 'championnat cadet europe 2020' ,2020-02-01,1),
+(NULL, 'championnat junior europe 2020' ,2020-02-01,3),
+(NULL, 'compet cadet france 2021' ,2021-02-01,1),
+(NULL, 'compet junior france 2021' ,2021-02-01,1),
+(NULL, 'competition cadet europe 2021' ,2021-02-01,1),
+(NULL, 'competition junior europe 2021' ,2021-02-01,3),
+(NULL, 'championnat cadet france 2021' ,2021-02-01,1),
+(NULL, 'championnat junior france 2021' ,2021-02-01,2),
+(NULL, 'championnat cadet europe 2021' ,2021-02-01,1),
+(NULL, 'championnat junior europe',2021-02-01,3);
+
+
+
+
+INSERT INTO classement VALUES
+(2,1,1),
+(1,1,2),
+(4,1,3),
+(4,2,4),
+(2,2,2),
+(6,2,3),
+(3,2,1),
+(1,2,5),
+(5,2,6),
+(3,3,1),
+(4,3,2),
+(5,3,2),
+(4,4,1),
+(2,4,2),
+(5,4,3),
+(2,5,1),
+(1,5,2),
+(4,5,3),
+(4,6,4),
+(2,6,2),
+(6,6,3),
+(3,6,1),
+(1,6,5),
+(5,6,6),
+(3,7,1),
+(4,7,2),
+(5,7,2),
+(4,8,1),
+(2,8,2),
+(5,8,3),
+(2,9,1),
+(1,9,2),
+(4,9,3),
+(4,10,4),
+(2,11,2),
+(6,11,12),
+(3,11,1),
+(1,11,5),
+(5,11,6),
+(3,12,1),
+(4,12,2),
+(5,12,2),
+(4,13,1),
+(2,13,2),
+(5,13,3),
+(7,23,1);
